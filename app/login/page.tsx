294bdedb815fd514c7a493aaf74ca595b94839e1
Mohammed Gharbi <mohammedgharbi@icloud.com>
2025-05-31 22:08:27 +0100

    login suspense

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,15 @@
-"use client"
-
+import { Suspense } from "react"
 import { LoginForm } from "./form"
 
 export default function LoginPage() {
   return (
-    <>
+    <Suspense fallback={<p>Loading...</p>}>
       <div className="h-screen flex flex-col justify-center items-center">
         <div className="w-96 text-center">
           <div className="text-3xl font-bold mb-8">Login</div>
           <LoginForm />
         </div>
       </div>
-    </>
+    </Suspense>
   )
 }
