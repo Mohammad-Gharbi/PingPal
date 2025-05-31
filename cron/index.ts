fc08188010b7b9b954d67bddffaabae968f009d3
Mohammed Gharbi <mohammedgharbi@icloud.com>
2025-05-31 21:19:23 +0100

    ping job

--- cron/index.ts
+++
@@ -1,7 +0,0 @@
-import cron from "node-cron"
-import { PingMonitors } from "./ping"
-
-cron.schedule("*/5 * * * *", async () => {
-  console.log("Running scheduled ping job...")
-  await PingMonitors()
-})
