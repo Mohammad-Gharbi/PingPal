1641deec2b4e7043447c8ba8ebfca659f2c64225
Mohammed Gharbi <mohammedgharbi@icloud.com>
2025-06-01 23:04:51 +0100

    suspense boundary

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,4 @@
-"use client"
-
+import { Suspense } from "react"
 import { LoginForm } from "./form"
 
 export default function LoginPage() {
@@ -7,7 +6,9 @@ export default function LoginPage() {
     <div className="h-screen flex flex-col justify-center items-center">
       <div className="w-96 text-center">
         <div className="text-3xl font-bold mb-8">Login</div>
-        <LoginForm />
+        <Suspense fallback={<div>Loading form...</div>}>
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
   )
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,4 @@
-"use client"
-
+import { Suspense } from "react"
 import { RegisterForm } from "./form"
 
 export default function RegisterPage() {
@@ -8,7 +7,9 @@ export default function RegisterPage() {
       <div className="h-screen flex flex-col justify-center items-center">
         <div className="w-96 text-center">
           <div className="text-3xl font-bold mb-8">Sign up</div>
-          <RegisterForm />
+          <Suspense fallback={<div>Loading form...</div>}>
+            <RegisterForm />
+          </Suspense>
         </div>
       </div>
     </>
